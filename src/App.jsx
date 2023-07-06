d52440f102dabc913835d64127c275002415c08e
aminul-islam01 <abiddut953@gmail.com>
2023-07-06 19:18:22 +0600

    add react project todo list

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Home from './Components/Home/Home/Home'
+import Products from './Components/Home/Products/Products'
+
+function App() {
+
+  return (
+    <div>
+      <Home></Home>
+      <Products></Products>
+    </div>
+  )
+}
+
+export default App
